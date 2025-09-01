7a8582c4c6cd5341224402d71d7a1bfcbbabd174
die-rod-go <43896095+die-rod-go@users.noreply.github.com>
2025-08-31 22:37:56 -0400

    changed default input and output strings

--- Pixel Sorting/Pixel Sorting/main.cpp
+++ Pixel Sorting/Pixel Sorting/main.cpp
@@ -12,17 +12,17 @@ int main() {
 
     //  image that's donating pixels
     BitmapImage image1;
-    if (!image1.load("C:\\Users\\diego\\Desktop\\photos\\datamosh\\brightness sorting\\input\\cloud1.bmp")) 
+    if (!image1.load("PUT IMAGE PATH ")) 
     {
-        std::cerr << "failed to load bmp1\\n";
+        std::cerr << "failed to load bmp1\n";
         return 1;
     }
 
     // image that will be recreated with those pixels
     BitmapImage image2;
-    if (!image2.load("C:\\Users\\diego\\Desktop\\photos\\datamosh\\brightness sorting\\input\\van_level.bmp")) 
+    if (!image2.load("PUT IMAGE PATH HERE")) 
     {
-        std::cerr << "failed to load bmp2\\n";
+        std::cerr << "failed to load bmp2\n";
         return 1;
     }
 
