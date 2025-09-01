23c84e579f05270c92321dbfbf51a2ba9197d61f
die-rod-go <43896095+die-rod-go@users.noreply.github.com>
2025-08-31 22:31:45 -0400

    added some comments and removed weird unused files

--- Pixel Sorting/Pixel Sorting/main.cpp
+++ Pixel Sorting/Pixel Sorting/main.cpp
@@ -4,29 +4,35 @@
 #include <time.h>
 #include <conio.h>
 #include "Bitmap.h"
-#include "ImageRecreator.h"
 #include "MosaicMaker.h"
 #include "RandomStuff.h"
 
 int main() {
 
 
+    //  image that's donating pixels
     BitmapImage image1;
-    if (!image1.load("C:\\Users\\diego\\Desktop\\photos\\datamosh\\brightness sorting\\input\\cloud1.bmp")) 
+    if (!image1.load("PUT IMAGE PATH HERE")) 
     {
         std::cerr << "failed to load bmp1\n";
         return 1;
     }
 
+    // image that will be recreated with those pixels
     BitmapImage image2;
-    if (!image2.load("C:\\Users\\diego\\Desktop\\photos\\datamosh\\brightness sorting\\input\\van_level.bmp")) 
+    if (!image2.load("PUT IMAGE PATH HERE")) 
     {
         std::cerr << "failed to load bmp2\n";
         return 1;
     }
 
+    // make output
     MosaicMaker mosaicMaker;
+    // i didn't go over this in the post but you can also grab and rearrange "blocks" of pixels
+    // instead of individual ones. so you can created cut up mosaics and stuff. it's hard to explain but
+    // 
+    // change this number                                        |
+    // and you'll see what I mean                                v
     BitmapImage image = mosaicMaker.createMosaic(image1, image2, 1);
-
-    image.save("C:\\Users\\diego\\Desktop\\photos\\datamosh\\brightness sorting\\output\\poopy.bmp");
+    image.save("PUT OUTPUT PATH AND NAME HERE");
 }
